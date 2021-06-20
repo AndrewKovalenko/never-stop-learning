75cf04a37d5de90a1b456d4be29dcd25a0c00606
Andrew Kovalenko <mr.kovalenko.a.o@gmail.com>
2021-06-20 04:42:52 -0700

    better solution for longest substring

--- longestSubstringNoRepetitiveChars/main.go
+++ longestSubstringNoRepetitiveChars/main.go
@@ -1,46 +1,34 @@
 package main
 
-import "fmt"
-
-func maxOf(a int, b int) int {
-	if a > b {
-		return a
-	} else {
-		return b
-	}
-}
-
-func searchSubstring(s string, foundSubstringLength int) int {
-	if len(s) == 0 {
-		return foundSubstringLength
-	}
-
-	substrLength := 0
-	charactersFound := make(map[rune]bool)
-
-	for i, char := range s {
-		if charactersFound[char] {
-			bestResultSoFar := maxOf(substrLength, foundSubstringLength)
-			substrLengthInTheRestOfTheString := searchSubstring(s[i:], bestResultSoFar)
-
-			return maxOf(substrLengthInTheRestOfTheString, bestResultSoFar)
-		} else {
-			charactersFound[char] = true
-			substrLength = substrLength + 1
-		}
-	}
-
-	return maxOf(substrLength, foundSubstringLength)
-}
+import (
+	"fmt"
+	"math"
+)
 
 func lengthOfLongestSubstring(s string) int {
-	return searchSubstring(s, 0)
+	maxLengthFound := 0
+	start := 0
+	charsFound := make(map[rune]bool)
+
+	for i, char := range s {
+		for charsFound[char] {
+			startRune := rune(s[start])
+			charsFound[startRune] = false
+			start = start + 1
+		}
+
+		charsFound[char] = true
+		length := i - start + 1
+		maxLengthFound = int(math.Max(float64(length), float64(maxLengthFound)))
+	}
+
+	return int(math.Max(float64(maxLengthFound), 0))
 }
 
 func main() {
 	fmt.Println(lengthOfLongestSubstring("abcabcbb"))
 	fmt.Println(lengthOfLongestSubstring("bbb"))
-	fmt.Println(lengthOfLongestSubstring("pwwkew"))
+	fmt.Println(lengthOfLongestSubstring("pwpker"))
 	fmt.Println(lengthOfLongestSubstring(""))
 	fmt.Println(lengthOfLongestSubstring("aab"))
 }
